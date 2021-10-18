9e4ae9a6701c7a7df7181aeacd3396688cd0ee63
Mariusz Gogola <mariuszgogola@gmail.com>
2021-10-18 16:03:33 +0200

    Dodaj pliki projektów.

---
+++ Project1/Project1.cpp
@@ -0,0 +1,25 @@
+﻿#include <iostream>
+using namespace std;
+
+struct uczniowie {
+    string imie;
+    string nazwisko;
+    int wiek;
+};
+
+
+int main()
+{
+    uczniowie u1;
+
+    cout << "Podaj imie";
+    cin >> u1.imie;
+    cout << "Podaj nazwisko";
+    cin >> u1.nazwisko;
+    cout << "Podaj wiek";
+    cin >> u1.wiek;
+
+    cout << "******************" << endl;
+    cout << "Imie: " << u1.imie << " Nazwisko: " << u1.nazwisko << " wiek: " << u1.wiek << endl;
+}
+
